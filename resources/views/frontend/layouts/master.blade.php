013046e12021a0fc11d5bdf19a0b5a09f372491e
pallavidots <92424856+pallavidots@users.noreply.github.com>
2026-03-03 14:34:54 +0530

    home page loader removed

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -49,44 +49,7 @@
     <!-- Footer -->
     @include('frontend.partials.footer')
 
-    <!-- Preloader Start -->
-    <div id="preloader-active">
-        <div class="preloader d-flex align-items-center justify-content-center">
-            <div class="preloader-inner position-relative">
-                <div class="text-center">
-                    <img src="{{ asset('frontend/imgs/theme/loading.gif') }}" alt="" />
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <style>
-        #preloader-active {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: linear-gradient(135deg, #3b95b7 0%, #2a6b8f 100%);
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            z-index: 9999;
-        }
-
-        #preloader-active .preloader {
-            background: transparent;
-        }
-
-        #preloader-active .preloader-inner {
-            animation: spin 2s linear infinite;
-        }
-
-        @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
-        }
-    </style>
 
     <!-- Scripts -->
     @include('frontend.partials.scripts')
